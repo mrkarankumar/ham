3f41b06d8de92517f6796bc80a5ad1757195ea54
Karan Kumar <149156353+mrkarankumar@users.noreply.github.com>
2024-07-24 02:32:12 +0530

    movies -recommendation- system

--- App (1).py
+++ App (1).py
@@ -62,7 +62,6 @@ st.set_page_config(page_title="Movie Recommender System")
 
 def run():
     st.title(" karan kumar")
-    # img1 = Image.open('meta/karan.jpg')
     img1 = Image.open('meta/logo.jpg')
     img1 = img1.resize((250, 250))
     st.image(img1, use_column_width=False)
