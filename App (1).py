968453c657db377328fa35245b10edbf10d31e6c
Karan Kumar <149156353+mrkarankumar@users.noreply.github.com>
2024-07-24 00:52:18 +0530

    movies recommendation system

--- App (1).py
+++ App (1).py
@@ -61,7 +61,7 @@ def KNN_Movie_Recommender(test_point, k):
 st.set_page_config(page_title="Movie Recommender System")
 
 def run():
-    st.title(" made by karan kumar")
+    st.title(" karan kumar")
     # img1 = Image.open('meta/karan.jpg')
     img1 = Image.open('meta/logo.jpg')
     img1 = img1.resize((250, 250))
